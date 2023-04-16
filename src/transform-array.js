d6999fec120e3b7feb661c6e91031c60e191e893
Milana Kasumava <milanamamedova52@mail.ru>
2023-04-16 22:34:33 +0300

    feat: finish arr transform task

--- src/transform-array.js
+++ src/transform-array.js
@@ -14,9 +14,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function transform(array) {
+  if (Array.isArray(array)){
+  let result=[];
+   for (let i=0;i<=array.length-1;i++){
+     if (array[i]=='--double-next') {
+       if (array.indexOf('--double-next')!=array.length-1){
+       result.push(array[i+1]);
+       }
+     } else if (array[i]=='--discard-next') {
+       if (array[i+2]=='--discard-prev' || array[i+2]=='--double-prev'){
+        i++
+        i++
+       } else {
+       i++
+       }
+     } else if (array[i]=='--discard-prev') {
+       if (array.indexOf('--discard-prev')!=0){
+       result.pop()
+       }
+     } else if (array[i]=='--double-prev' ) {
+       if (array.indexOf('--double-prev')!=0){
+         result.push(result[result.length-1]);
+  
+       }
+     } else {
+       result.push(array[i])
+     }
+   }
+   return result
+  } else {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+ }
+  
 
-  throw new NotImplementedError('Not implemented');
-}
 
 
 module.exports = {
